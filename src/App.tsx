b8dd864accb0a7c793120ed7de1d7b769660be2d
Guilherme Di Paula <guilepaul@gmail.com>
2020-10-20 23:50:40 -0300

    signup screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import SignIn from './Signin';
+import SignUp from './Signup';
 
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <SignUp />
     <GlobalStyle />
   </>
 );
