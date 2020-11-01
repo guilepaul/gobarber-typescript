f776424fb2d6e97a674b6958be84c5e19744094d
Guilherme Di Paula <guilepaul@gmail.com>
2020-11-01 18:50:08 -0300

    context api

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,13 @@ import SignUp from './Signup';
 
 import GlobalStyle from './styles/global';
 
+import AuthContext from './context/AuthContext';
+
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <AuthContext.Provider value={{ name: 'Guilherme' }}>
+      <SignIn />
+    </AuthContext.Provider>
     <GlobalStyle />
   </>
 );
