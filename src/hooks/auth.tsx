462e17b72fd6b4bf71095a7f85e02ded5c7e0889
Guilherme Di Paula <guilepaul@gmail.com>
2020-11-22 20:54:28 -0300

    fix auth hook

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -45,8 +45,8 @@ const AuthProvider: React.FC = ({ children }) => {
   }, []);
 
   const signOut = useCallback(() => {
-    localStorage.remove('@GoBarber:token');
-    localStorage.remove('@GoBarber:user');
+    localStorage.removeItem('@GoBarber:token');
+    localStorage.removeItem('@GoBarber:user');
 
     setData({} as AuthState);
   }, []);
